7be4636c6a5a85ee9a517d5ebc868df7c33a20ac
Alexander Zhidkov <azhidkov@gmail.com>
2023-11-17 13:45:56 +0800

    cloned from template

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDxJMGHUyKzdSqQI6mxNUNjDEzAIjEjkVI",
+    authDomain: "creators-funds.firebaseapp.com",
+    projectId: "creators-funds",
+    storageBucket: "creators-funds.appspot.com",
+    messagingSenderId: "803830730162",
+    appId: "1:803830730162:web:e35ef2859b7207894268ab",
+    measurementId: "G-2Z2475RNEF"
+  },
+};
