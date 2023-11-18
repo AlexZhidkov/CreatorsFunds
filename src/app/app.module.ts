dc928112948618000a3eec0975b8e241cb839868
Alexander Zhidkov <azhidkov@gmail.com>
2023-11-18 16:40:45 +0800

    channels info cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,6 +44,7 @@ import { MatChipsModule } from '@angular/material/chips';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -95,7 +96,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatSnackBarModule,
     MatChipsModule,
     MatAutocompleteModule,
-    MatSelectModule
+    MatSelectModule,
+    MatExpansionModule
   ],
   providers: [
     ScreenTrackingService, UserTrackingService
